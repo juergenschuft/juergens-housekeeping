3e83638dda8f925cccd464a86e0517c0ed18163e
juergenschuft <juergen.schuft@web.de>
2020-05-03 00:14:30 +0200

    new backup script using link-dest option

--- backup/backupDatenGesichert.sh
+++ backup/backupDatenGesichert.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# inspiration: https://blog.mdosch.de/2013/08/01/meine-neue-backup-loesung/
+
 if [ "$EUID" -ne 0 ]
   then echo "bitte mit sudo starten"
   exit
@@ -8,13 +10,32 @@ fi
 echo "### los gehts ###"
 
 LOGFILE="$0.log"
+LOGLASTEXEC="lastexec.log"
 SECONDS=0
 
 dt=$(date '+%Y-%m-%d %H:%M:%S')
 
+# echo "$dt" # $LOGLASTEXEC
+
 echo "$dt - starte backup" >> $LOGFILE
 
-rsync -rtvP --size-only DatenGesichert/* bakAuto/ShuttleDriveE/
+# rsync -rtvP --size-only --exclude _uploadDir --exclude _fotosArchivieren DatenGesichert/* bakAuto/ShuttleDriveE/
+
+# dt=$(date '+%Y-%m-%d %H:%M:%S')
+
+BAKID="$(date '+%Y%m%d%H%M%S')"
+
+BAKIDLAST="$(<$LOGLASTEXEC)"
+
+echo "BAKID: $BAKID"
+
+echo "BAKIDLAST: $BAKIDLAST"
+
+mkdir "snapshots/$BAKID"
+
+rsync -azvP --delete --size-only --exclude backupman --exclude _uploadDir --exclude _fotosArchivieren --link-dest=../$BAKIDLAST DatenGesichert/* snapshots/$BAKID/ 2>> $LOGFILE
+
+echo "$BAKID" > $LOGLASTEXEC
 
 dt=$(date '+%Y-%m-%d %H:%M:%S')
 
